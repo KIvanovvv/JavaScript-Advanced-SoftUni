855e8eacf9a62b850a35fde11f54145b1f424002
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-12 20:08:01 +0100

    Add homework

--- Syntax,FunctionsAndStatements/stringLength.js
+++ Syntax,FunctionsAndStatements/stringLength.js
@@ -1,8 +1,6 @@
 function stringConvert(str1, str2, str3) {
   let arr = [];
-  arr.push(str1);
-  arr.push(str2);
-  arr.push(str3);
+  arr.push(str1, str2, str3);
   const length = arr.join("").length;
   const avgL = Math.floor(length / 3);
   console.log(length);

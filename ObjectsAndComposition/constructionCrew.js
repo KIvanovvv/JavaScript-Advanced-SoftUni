621d7ad0aadc4ce88bd37076b0a41f80766ab81f
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-20 19:45:50 +0100

    Add homework

---
+++ ObjectsAndComposition/constructionCrew.js
@@ -0,0 +1,16 @@
+function constructionCrew(obj) {
+  if (obj.dizziness) {
+    obj.levelOfHydrated = obj.weight * 0.1 * obj.experience;
+    obj.dizziness = false;
+  }
+  return obj;
+}
+constructionCrew({
+  weight: 80,
+
+  experience: 1,
+
+  levelOfHydrated: 0,
+
+  dizziness: true,
+});

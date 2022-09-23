6c6836893c8af7f1cb7086ebd33e88ea20b9ac61
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-23 15:37:44 +0100

    Add homework

---
+++ Dom/colorizeTable/ColorizeTable.js
@@ -0,0 +1,6 @@
+function colorize() {
+  let rows = document.querySelectorAll(`table tr`);
+  for (let i = 0; i < rows.length; i++) {
+    if (i % 2 != 0 && i != 0) rows[i].style.backgroundColor = `teal`;
+  }
+}

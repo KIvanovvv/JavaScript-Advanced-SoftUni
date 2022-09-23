e4496e4778ebab82ad44a08ddd99b649e14d1702
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-23 15:07:52 +0100

    Add homework

---
+++ ObjectsAndComposition/Dom/editElement/02.Collect List Items/sumNumbers/shwoMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+  let moreText = document.getElementById(`text`);
+  let readMoreBtn = document.getElementById(`more`);
+  readMoreBtn.style.display = `none`;
+  moreText.style.display = `inline`;
+}

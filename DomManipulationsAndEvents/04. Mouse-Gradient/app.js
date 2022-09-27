361c3b833fea3cce3d2975aae37bd41ae72a0e39
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-27 16:52:50 +0100

    Add homework

--- DomManipulationsAndEvents/04. Mouse-Gradient/app.js
+++ DomManipulationsAndEvents/04. Mouse-Gradient/app.js
@@ -1,10 +1,14 @@
 function attachGradientEvents() {
   const gradient = document.querySelector(`#gradient`);
+  let res = document.querySelector(`#result`);
   gradient.addEventListener(`mousemove`, (e) => {
     let position = e.offsetX;
-    let gradientWidth = e.target.offsetWidth;
+    let gradientWidth = e.target.clientWidth - 1;
     let precentage = Math.floor((position / gradientWidth) * 100);
 
-    document.querySelector(`#result`).textContent = `${precentage}%`;
+    res.textContent = precentage + "%";
+  });
+  gradient.addEventListener(`mouseout`, () => {
+    res.textContent = ``;
   });
 }
---
+++ DomManipulationsAndEvents/05. Highlight-Active/app.js
@@ -0,0 +1,13 @@
+function focused() {
+  const inputs = document.getElementsByTagName(`input`);
+  for (let input of inputs) {
+    input.addEventListener(`focus`, focusInput);
+    input.addEventListener(`blur`, blurInput);
+  }
+  function focusInput(event) {
+    event.target.parentElement.classList.add(`focused`);
+  }
+  function blurInput(event) {
+    event.target.parentElement.classList.remove(`focused`);
+  }
+}

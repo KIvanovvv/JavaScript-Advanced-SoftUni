dd9532d12a022b0e20ce8a93b3d4bca06c500814
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-10-01 18:28:19 +0300

    Add homework

---
+++ AdvancedFunctions/Fibinocci.js
@@ -0,0 +1,26 @@
+function getFibonator() {
+  let a = 0;
+  let b = 1;
+  return () => {
+    let res = a + b;
+    a = b;
+    b = res;
+    return a;
+  };
+}
+
+let fib = getFibonator();
+
+console.log(fib()); // 1
+
+console.log(fib()); // 1
+
+console.log(fib()); // 2
+
+console.log(fib()); // 3
+
+console.log(fib()); // 5
+
+console.log(fib()); // 8
+
+console.log(fib());
